0cb86a5cfbe7c101337e564485cf2192f7ee4d6a
Umadevi695 <uma970179@gmail.com>
2025-05-16 19:24:40 +0530

    updated

--- a11/index.js
+++ a11/index.js
@@ -1,9 +1,4 @@
-let a = 10;
-let b = 20;
-if (a > b) {
-  console.log("a is greater");
-} else if (b > a) {
-  console.log("b is greater");
-} else {
-  console.log("both a and b are equal");
+for(var i=1;i<=10;i++){
+    if(i%2!=0)
+        console.log(i);
 }
