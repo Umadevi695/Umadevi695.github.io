fcb430f64b97ec35e70b021d564a5a4243b1b13a
Umadevi695 <uma970179@gmail.com>
2025-05-18 19:43:05 +0530

    updated

---
+++ p2/script.js
@@ -0,0 +1,25 @@
+const products = [
+  { id: 1, name: "p1", price: 34 },
+  { id: 2, name: "p2", price: 50 },
+  { id: 3, name: "p3", price: 75 },
+];
+
+const showProducts = () => {
+  const root = document.getElementById("root");
+
+  root.innerHTML = "<h2>Product List</h2>"; // Set initial heading
+
+  products.map((value) => {
+    root.innerHTML += `
+      <div class="product">
+        <p>${value.name}</p>
+        <p>₹${value.price}</p>
+        <button onclick="addToCart(${value.id})">Add to cart</button>
+      </div>
+    `;
+  });
+};
+
+const addToCart = (id) => {
+  alert("Product " + id + " added to cart!");
+};
